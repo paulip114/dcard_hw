8167f410a7270b06dc1f00bd310aa3f12332d9ea
paulip114 <paulip114@gmail.com>
2021-04-07 12:35:52 +0800

    infinite-scroll sucess

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,49 +1,108 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
+
 //import componets
 import InfoCard from '../components/InfoCard.jsx';
 import Navbar from '../components/Navbar.jsx';
+import CityList from '../components/CityList.jsx';
+import InfiniteScroll from "react-infinite-scroll-component";
 //import axios request
-import { getScenicSpotRequest } from '../ScenicSpotRequest.js';
+// import RequestTest from '../RequestTest.js';
+import axios from 'axios';
+// import { getScenicSpotRequest } from '../ScenicSpotRequest.js';
 //import styles
 import '../App.css';
 
 
 function MainScreen() {
 
-  //使用useState 和 useEffect
-  //將從getScenicSpotRequest()傳過來的axios promise data 存進 setSceneResults
-  const [sceneResults, setSceneResults] = useState([]);
-  useEffect(() => {
-    getScenicSpotRequest().then(setSceneResults);
-  }, [setSceneResults]);
+    const [ScenicSpot, setScenicSpot] = useState([])
+    // const [PrevScenicSpot, setPrevScenicSpot] = useState([])
+    const topNdata = 30
+    const [skipNdata, setSkipNdata] = useState(0)
+    const [requestTimes, setRequestTimes] = useState(1)
+    const [firstRendered, setfirstRender] = useState(false)
 
-  //拉到最底刷新axios request 功能（尚未完成）
-  // const [] = useState();
-  // useEffect(() => {
-  //   if (this.scroll) {
-  //     this.scroll.addEventListener("scroll", res => {
-  //       const { clientHeight, scrollHeight, scrollTop } = res.target;
-  //       // const { clientHeight, scrollHeight, scrollTop } = this.scroll;
+    if (firstRendered === false) {
+        getFristScenicSpotRequest(topNdata, firstRendered).then(setScenicSpot);
+        setfirstRender(true)
+        setSkipNdata(requestTimes * 30)
+        console.log("first render ended")
+    }
 
-  //       const isBottom = scrollTop + clientHeight + 20 > scrollHeight;
-  //       console.log(scrollTop, clientHeight, scrollHeight, isBottom);
-  //     });
-  //   }
-  // });
+    const RequestHandler = () => {
+        if (firstRendered === true) {
+            // setPrevScenicSpot(ScenicSpot);
+            getScenicSpotRequest(topNdata, skipNdata).then(res => {
+                setScenicSpot(PrevSpot => {
+                    return [...new Set([...PrevSpot, ...res])]
+                })
+            });
+            console.log("sent second request");
+            console.log(ScenicSpot)
+            setRequestTimes(requestTimes + 1);
+            setSkipNdata(requestTimes * 30);
+            console.log("requestTimes:", requestTimes);
+        }
+    }
 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Navbar NavbarTitle="全部景點列表" />
+                <CityList />
+                <InfiniteScroll
+                    dataLength={ScenicSpot.length}
+                    next={RequestHandler}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                >
+                    {ScenicSpot.map((infoCard,index) => (<InfoCard key={index} Name={infoCard.Name} Description={infoCard.Description} Picture={infoCard.Picture.PictureUrl1} />))}
+                </InfiniteScroll>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar NavbarTitle="全部景點列表" />
-        {sceneResults.map((infoCard) => (<InfoCard key={infoCard.ID} Name={infoCard.Name} Description={infoCard.Description} Picture={infoCard.Picture.PictureUrl1} />))}
-        {/* <div ref={res => (this.scroll = res)}></div> 拉到最底刷新axios request 功能（尚未完成）*/}
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
+}
+
+const Request = axios.create({
+    baseURL: 'https://ptx.transportdata.tw/MOTC/v2/Tourism/'
+});
+
+async function getFristScenicSpotRequest(topNdata, firstRendered) {
+    if (firstRendered === false) {
+        try {
+            const item = await Request.get('ScenicSpot', { params: { $top: topNdata, $format: 'JSON' } }).then(response => response.data);
+            console.log("sent Frist request");
+            return item;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+}
+
+async function getScenicSpotRequest(topNdata, skipNdata) {
+    console.log("skip ScenicSpotRequest:", skipNdata)
+    try {
+        const item = await Request.get('ScenicSpot',
+            {
+                params: { $top: topNdata, $skip: skipNdata, $format: 'JSON' }
+            })
+            .then(response => response.data)
+        // .catch(e => {
+        //     if (axios.isCancel(e)) return
+        //     setError(true)
+        // });
+        return item;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 export default MainScreen;
+
+
+
 
 
 
